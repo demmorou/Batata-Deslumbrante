3f250ef17a295917f05ab026cb980fbb3b113eb6
Deusimar <34683933+deusimar123@users.noreply.github.com>
2018-05-10 09:00:36 -0300

    Update ModelTrain.py

--- ModelTrain.py
+++ ModelTrain.py
@@ -16,17 +16,26 @@ Parâmetros 'flow_from_directory'
 --------------------------------
 
 	'data_set/train': pasta que contém os dados de treino (as imagens) devidamente separados de acordo com seus rótulos.
-	target_size: tupla de inteiros (altura, largura), por padrão: (256, 256). As dimensões para as quais todas as imagens encontradas serão 	redimensionadas.
+	target_size: tupla de inteiros (altura, largura), por padrão: (256, 256). As dimensões para as quais todas as imagens 
+	encontradas serão redimensionadas.
 	batch_size: tamanho dos lotes de dados.
-	class_mode: Determina o tipo de matrizes de rótulos da saída: "categorical" serão rótulos codificados em 2D, "binary" serão rótulos binários 		de 1D, "esparsos" serão rótulos inteiros de 1D, "input" serão imagens idênticas às imagens de entrada (usado principalmente para trabalhar 		com autoencodificadores).
+	class_mode: Determina o tipo de matrizes de rótulos da saída: "categorical" serão rótulos codificados em 2D, "binary" 
+	serão rótulos binários 	de 1D, "esparsos" serão rótulos inteiros de 1D, "input" serão imagens idênticas às imagens de 
+	entrada (usado principalmente para trabalhar com autoencodificadores).
 	
 Parâmetros 'fit_generator'
 -------------------------------
 	
-	steps_per_epochs: Número total de etapas (lotes de amostras) antes de declarar uma época concluída e iniciar a próxima época. Ao treinar com 		tensores de entrada, como tensores de dados TensorFlow, o padrão None igual ao número de amostras em seu conjunto de dados dividido pelo 		tamanho do lote ou 1, se isso não puder ser determinado.
-	epochs: Número de épocas para treinar o modelo. Uma época é uma iteração sobre o todo x e os y dados fornecidos. O modelo não é treinado para 		um número de iterações dadas por epochs, mas meramente até que a época do índice epochs seja alcançada.
-	verbose: 0, 1 ou 2. Modo de verbosidade. 0 = silencioso, 1 = barra de progresso, 2 = uma linha por época.
-	validation_data: tupla contendo (x_val, y_val) no qual irá avaliar a perda e qualquer métrica de modelo no final de cada época. O modelo não 		será treinado nesses dados. Isso irá substituir validation_split, que pode ser definido como 0 e 1 e funciona de maneira semelhante.
+	steps_per_epochs: Número total de etapas (lotes de amostras) antes de declarar uma época concluída e iniciar a próxima 
+	época. Ao treinar com tensores de entrada, como tensores de dados TensorFlow, o padrão None igual ao número de amostras 
+	em seu conjunto de dados dividido pelo tamanho do lote ou 1, se isso não puder ser determinado.
+	epochs: Número de épocas para treinar o modelo. Uma época é uma iteração sobre o todo x e os y dados fornecidos. 
+	O modelo não é treinado para um número de iterações dadas por epochs, mas meramente até que a época do índice epochs 
+	seja alcançada. verbose: 0, 1 ou 2. Modo de verbosidade. 0 = silencioso, 1 = barra de progresso, 2 = uma linha por 
+	época. 
+	validation_data: tupla contendo (x_val, y_val) no qual irá avaliar a perda e qualquer métrica de modelo no final de cada 
+	época. O modelo não será treinado nesses dados. Isso irá substituir validation_split, que pode ser definido como 0 e 1 e 
+	funciona de maneira semelhante.
 	
 '''
 
